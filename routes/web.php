a991a8da017e99470da6a7ae014732fe3e9c9ab8
MohammedSaeid <183497215+MoZayedSaeid@users.noreply.github.com>
2025-07-01 23:57:13 +0200

    improvments in ui and fixes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/home', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->get('/dashboard', function () {
+
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
